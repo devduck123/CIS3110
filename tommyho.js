6c3d0a9f604df84c139fde170e7d0246c671f24d
tommy <tommyho@cpp.edu>
2021-09-25 17:39:54 -0700

    added hw#5 feature

--- tommyho.js
+++ tommyho.js
@@ -1,31 +1,59 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').onsubmit = () => {
-        fetch('http://api.exchangeratesapi.io/v1/latest?access_key=78c5d28e78cb6dc299c4b898d3b495f7')
-        .then(response => response.json())
-        .then(data => {
-            // sanitize user input
-            const currency = document.querySelector('#currency').value.toUpperCase();
+document.addEventListener("DOMContentLoaded", () => {
+  // live loading of userID via #name
+  document.querySelector("#name").onkeyup = () => {
+    let name = document.querySelector("#name").value;
+    let number = document.querySelector("#number").value;
 
-            // get value corresponding to key of currency
-            const rate = data.rates[currency];
+    let userID = (name + "" + number).replace(/\s+/g, "");
 
-            // for assignment -- required to log an input
-            console.log(currency);
-            console.log(rate);
+    document.querySelector("#user-id").innerHTML = userID;
+    document.querySelector("#user-id").style.display = "block";
+  };
 
-            if (rate !== undefined) {
-                document.querySelector('#currency_result').innerHTML = `1 EUR is equal to ${rate} ${currency}`;
-            }
-            else {
-                document.querySelector('#currency_result').innerHTML = 'Unable to convert that currency.';
-            }
-        })
+  // live loading for userID via #number
+  document.querySelector("#number").onkeyup = () => {
+    let name = document.querySelector("#name").value;
+    let number = document.querySelector("#number").value;
 
-        // catch errors with fetching API
-        .catch(error => {
-            console.log('Error: ', error);
-        });
+    let userID = (name + "" + number).replace(/\s+/g, "");
 
-        return false;
-    }
+    document.querySelector("#user-id").innerHTML = userID;
+    document.querySelector("#user-id").style.display = "block";
+  };
+
+  document.querySelector("form").onsubmit = () => {
+    fetch(
+      "http://api.exchangeratesapi.io/v1/latest?access_key=78c5d28e78cb6dc299c4b898d3b495f7"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // sanitize user input
+        const currency = document
+          .querySelector("#currency")
+          .value.toUpperCase();
+
+        // get value corresponding to key of currency
+        const rate = data.rates[currency];
+
+        // for assignment -- required to log an input
+        console.log(currency);
+        console.log(rate);
+
+        if (rate !== undefined) {
+          document.querySelector(
+            "#currency_result"
+          ).innerHTML = `1 EUR is equal to ${rate} ${currency}`;
+        } else {
+          document.querySelector("#currency_result").innerHTML =
+            "Unable to convert that currency.";
+        }
+      })
+
+      // catch errors with fetching API
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+
+    return false;
+  };
 });
